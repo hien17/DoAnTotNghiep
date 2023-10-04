2adb7d8f9dadd2afa2ad42e8220da58cb2a64ee9
hien.hoangwerner@hcmut.edu.vn <hien.hoangwerner@hcmut.edu.vn>
2023-10-05 00:44:17 +0700

    add blank page and components

---
+++ testContract/thirdweb-app/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Booking } from "./Booking";
+export { default as Management } from "./Management";
+export { default as History } from "./History";
