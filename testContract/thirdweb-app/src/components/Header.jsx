50ace0f205f51d18be3ce975276d146b7b72744c
hien.hoangwerner@hcmut.edu.vn <hien.hoangwerner@hcmut.edu.vn>
2023-10-06 01:50:34 +0700

    add UI show nft info in body of management

---
+++ testContract/thirdweb-app/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { ConnectWallet } from '@thirdweb-dev/react'
+import logo from "../assets/logo/logo.png";
+const Header = () => {
+  const pageNameStyle = "m-auto text-2xl font-[1000] bg-gradient-to-r from-sky-400 via-indigo-300 to-cyan-400 text-transparent bg-clip-text ";
+  return (
+    <div className="header p-4 border-b-2 ml-4">
+      <div className="flex gap-4 w-full justify-between">
+        <div>
+          <img className='w-20' src={logo}></img>
+        </div>
+        <div className="flex justify-between gap-16
+        ">
+            <div className={`${pageNameStyle}`}>
+              Home
+            </div>
+            <div className={`${pageNameStyle}`}>
+              Booking
+            </div>
+            <div className={`${pageNameStyle}`}>
+              Management
+            </div>
+            <div className={`${pageNameStyle}`}>
+              History
+            </div>
+        </div>
+        <div className="flex">
+          <ConnectWallet ></ConnectWallet>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- testContract/thirdweb-app/src/views/Login/Inner.jsx
+++ testContract/thirdweb-app/src/views/Login/Inner.jsx
@@ -9,21 +9,18 @@ import {
     useContract,
     useContractRead,
   } from "@thirdweb-dev/react";
+  import { Header } from 'components';
+  import { shortenAddress } from 'utils/shortenAddress';
 
 const Inner = memo(({ handleLogin }) => {
-const address = useAddress();
+  const address = useAddress();
   const contractAddress = "0x7e15935f8ae6FCbBAc0211D2E15A2166143707B3";
-  const tokenId = 3;
+  const [tokenId,setTokenId] = useState(3);
   const { contract } = useContract(contractAddress);
   const { data, isLoading } = useContractRead(contract, "getNFTInfo", [tokenId]);
 
   const [buttonClicked, setButtonClicked] = useState(false);
 
-  // useEffect(() => {
-  //   if (!isLoading && data) {
-  //     setButtonClicked(true);
-  //   }
-  // }, [isLoading, data]);
   const formatDate = (timestamp) => {
     const date = new Date(timestamp * 1000); // Multiply by 1000 to convert seconds to milliseconds
     const options = {
@@ -45,64 +42,116 @@ const address = useAddress();
 
   return (
     <>
-    <div className="header p-4 border-b-2 ml-4">
-      <div className="flex gap-4 w-full justify-between">
-        <div className="flex justify-between gap-16
-        ">
-            <div className="m-auto text-5xl font-[1000]
-        bg-gradient-to-r from-sky-400 via-indigo-500 to-cyan-400 text-transparent 
-        bg-clip-text ">
-              Logo
-            </div>
-            <div className="m-auto text-5xl font-[1000]
-        bg-gradient-to-r from-sky-400 via-indigo-500 to-cyan-400 text-transparent 
-        bg-clip-text ">
-              Home
-            </div>
-            <div className="m-auto text-5xl font-[1000]
-        bg-gradient-to-r from-sky-400 via-indigo-500 to-cyan-400 text-transparent 
-        bg-clip-text ">
-              Booking
-            </div>
-            <div className="m-auto text-5xl font-[1000]
-        bg-gradient-to-r from-sky-400 via-indigo-500 to-cyan-400 text-transparent 
-        bg-clip-text ">
-              Management
-            </div>
-            <div className="m-auto text-5xl font-[1000]
-        bg-gradient-to-r from-sky-400 via-indigo-500 to-cyan-400 text-transparent 
-        bg-clip-text ">
-              History
-            </div>
-        </div>
-        <div className="flex">
-          <ConnectWallet ></ConnectWallet>
-        </div>
-      </div>
-      
-      
-    </div>
+    <Header></Header>
     <div className="body border-[5px] border-cyan-400 p-20 m-20 rounded-xl
    space-y-10 ">
-      <div className="grid grid-cols-2 w-full ">
-        <div className="text-3xl font-extrabold text-sky-800 ">
+      <div className="flex flex-row justify-between">
+        <div className="text-3xl font-extrabold text-sky-600 ">
         Contract Address
         </div>
         <div className="font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
-               bg-clip-text">
-            {contractAddress}
+               bg-clip-text w-fit">
+            {contractAddress} 
         </div>
       </div>
-      <div className="grid grid-cols-2 w-full">
-        <div className="text-3xl font-extrabold text-sky-800 ">
+      <div className="flex flex-row justify-between ">
+        <div className="text-3xl font-extrabold text-sky-600 ">
         Number of room
         </div>
         <div className="font-[1000] text-5xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
-               bg-clip-text">
+               bg-clip-text w-fit">
           10
         </div>
       </div>
-      
+      <div className="py-6 flex flex-col justify-center  ">
+        <div className='flex justify-center pb-6'>
+          <input className="border-y-2 border-l-2 rounded-l-xl p-6 
+          font-[1000] text-xl border-cyan-500
+          bg-gradient-to-r from-sky-500 to-purple-500 
+          text-transparent bg-clip-text w-fit"
+          type="number">
+
+          </input>
+          <button 
+          onClick={(e) => setButtonClicked(!buttonClicked)}
+          className="border-y-2 border-r-2 p-6 rounded-r-xl border-sky-500"
+          onchange={(e)=>setTokenId(e.target.value)}>
+            <a className="font-[1000] text-xl 
+            bg-gradient-to-r from-sky-500 to-purple-500 
+            text-transparent bg-clip-text w-fit">
+              SHOW NFT INFO</a>
+          </button>
+        </div>
+        {/* Conditionally render NFT info based on the buttonClicked state */}
+        {isLoading && <p>Loading data...</p>}
+        {buttonClicked && data && !isLoading && (
+          <div className='border-2 p-16 space-y-16 border-cyan-500 rounded-xl'>
+          <div className='flex flex-row justify-between gap-4'>
+            <div className='w-full'>
+              <p className='flex justify-center text-3xl font-extrabold text-sky-600'>
+                Provider
+              </p>
+              <p className='flex justify-center 
+              font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
+              bg-clip-text '>{shortenAddress(data[0])}</p>
+            </div>
+            <div className='w-full'>
+              <p className='flex justify-center text-3xl font-extrabold text-sky-600'>
+                Renter
+              </p>
+              <p className='flex justify-center 
+              font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
+              bg-clip-text '>{shortenAddress(data[1])}</p>
+            </div>
+          </div>
+          <div className='flex flex-row justify-between gap-4'>
+            <div className='w-full'>
+              <p className='flex justify-center text-3xl font-extrabold text-sky-600'>
+                Room ID
+              </p>
+              <p className='flex justify-center 
+              font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
+              bg-clip-text '>{parseBigNumber(data[2])}</p>
+            </div>
+            <div className='w-full'>
+              <p className='flex justify-center text-3xl font-extrabold text-sky-600'>
+                Rent Amount
+              </p>
+              <p className='flex justify-center
+              font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
+              bg-clip-text'>{parseBigNumber(data[3])} $</p>
+            </div>
+          </div>
+          <div className='flex flex-row justify-between gap-4'>
+            <div className='w-full'>
+              <p className='flex justify-center text-3xl font-extrabold text-sky-600'>
+                Seconds Until Start Time
+              </p>
+              <p className='flex justify-center
+              font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
+              bg-clip-text'>{parseBigNumber(data[5])} seconds</p>
+            </div>
+            <div className='w-full'>
+              <p className='flex justify-center text-3xl font-extrabold text-sky-600'>
+                Duration
+              </p>
+              <p className='flex justify-center
+              font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
+              bg-clip-text'>{parseBigNumber(data[6])} seconds</p>
+            </div>
+          </div>
+          <div className='justify-center'>
+            <p className='flex w-full justify-center text-3xl font-extrabold text-sky-600'>
+              Start Time
+            </p>
+            <p className='flex w-full justify-center
+            font-[1000] text-4xl bg-gradient-to-r from-sky-500 to-purple-500 text-transparent 
+            bg-clip-text'>{formatDate(parseBigNumber(data[4]))}</p>
+          </div>
+        </div>
+        
+        )}
+      </div>
     </div>
     </>
   );
