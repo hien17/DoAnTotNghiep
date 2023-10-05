d1985559a571f3e6e26c309641b0b68ded33a915
hien.hoangwerner@hcmut.edu.vn <hien.hoangwerner@hcmut.edu.vn>
2023-10-05 21:14:43 +0700

    add UI show nft info in body of management

--- testContract/thirdweb-app/src/App.js
+++ testContract/thirdweb-app/src/App.js
@@ -10,7 +10,7 @@ import {
 export default function Home() {
   const address = useAddress();
   const contractAddress = "0x7e15935f8ae6FCbBAc0211D2E15A2166143707B3";
-  const tokenId = 3;
+  const [tokenId,setTokenId] = useState(3);
   const { contract } = useContract(contractAddress);
   const { data, isLoading } = useContractRead(contract, "getNFTInfo", [tokenId]);
 
@@ -167,7 +167,38 @@ export default function Home() {
           10
         </div>
       </div>
-      
+      <div className="p-6">
+        <input className="border-y-2 border-l-2 rounded-l-xl p-6 
+        font-[1000] text-xl 
+        bg-gradient-to-r from-sky-500 to-purple-500 
+        text-transparent bg-clip-text "
+        type="number">
+
+        </input>
+        <button 
+        onClick={(e) => setButtonClicked(!buttonClicked)}
+        className="border-y-2 border-r-2 p-6 rounded-r-xl"
+        onchange={(e)=>setTokenId(e.target.value)}>
+          <a className="font-[1000] text-xl 
+          bg-gradient-to-r from-sky-500 to-purple-500 
+          text-transparent bg-clip-text">
+            SHOW NFT INFO</a>
+          </button>
+
+        {/* Conditionally render NFT info based on the buttonClicked state */}
+        {isLoading && <p>Loading data...</p>}
+        {buttonClicked && data && !isLoading && (
+          <div>
+            <p>Provider: {data[0]}</p>
+            <p>Renter: {data[1]}</p>
+            <p>Room ID: {parseBigNumber(data[2])}</p>
+            <p>Rent Amount: {parseBigNumber(data[3])} $</p>
+            <p>Start Time: {formatDate(parseBigNumber(data[4]))}</p>
+            <p>Seconds Until Start Time: {parseBigNumber(data[5])} seconds</p>
+            <p>Duration: {parseBigNumber(data[6])} seconds</p>
+          </div>
+        )}
+      </div>
     </div>
     </>
   );
