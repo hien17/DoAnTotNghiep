459e3c81ba9effefc072688ca4c4196be51f0903
Phạm Duy Quang <75131380+ULTIMATE-Mystery@users.noreply.github.com>
2023-10-05 13:05:27 +0700

    Add tailwind.css

--- testContract/thirdweb-app/src/views/Hello/Inner.jsx
+++ testContract/thirdweb-app/src/views/Hello/Inner.jsx
@@ -1,7 +1,12 @@
 import { memo } from 'react';
+import "dist/output.css";
 
 const Inner = memo(() => {
-    return <div>Hello</div>;
+  return (
+  <div className="text-10xl font-extrabold text-sky-800 ">
+  Contract Address
+  </div>
+  );
 });
 
 Inner.displayName = 'Hello Inner';
